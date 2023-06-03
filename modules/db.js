d3182a8b816f61b487ec4c86459fe409808ed423
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-03 04:01:20 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -40,7 +40,7 @@ function connect(callback){
 */
 var db = require('mongodb').Db;
 var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/bothwellbot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 function connect(callback){
   MongoClient.connect(uri, function(err, db) {  
 //  const collection = client.db("test").collection("devices");  
