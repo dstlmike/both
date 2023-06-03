ae9dfcd2f4c96d844cf2ef3c84760cde5c68b3e9
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-03 02:20:43 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -20,7 +20,7 @@ var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha
 //}
 
 //var connection_string = mongodb+srv://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-esmha.mongodb.net/test?retryWrites=true&w=majority
-
+/*
 if(process.env.MONGODB_PASSWORD){
   // //connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
    connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/bothwellbot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
@@ -29,7 +29,7 @@ if(process.env.MONGODB_PASSWORD){
   process.env.MONGODB_SERVICE_PORT + '/' +
   process.env.MONGODB_DATABASE;
 }
-
+*/
 
 function connect(callback){
   mongoDB.connect(connection_string, function(err, db) {
