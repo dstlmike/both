612063ac524e9936a15ce042ab018bd34a56d79c
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-04 17:21:24 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -42,7 +42,7 @@ function connect(callback){
 //var dbt = MongoClient.connect(uri, function(err, db) {;
 var db = require('mongodb').Db;
 //var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://bot:bot@ac-6ymsztq-shard-00-00.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-01.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-02.lmt2mtk.mongodb.net:27017/?ssl=true&replicaSet=atlas-9aqeym-shard-0&authSource=admin&retryWrites=true&w=majority";
 //MongoClient.connect(uri, function(err, client) {
 function connect(callback){
   mongoDB.connect(uri, function(err, db) {  
