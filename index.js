9a4019d2ecc69071ad49ed8f6a5e6c09f278b435
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-04 02:37:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ server = http.createServer(function (req, res) {
 port = Number(process.env.NODEJS_SERVICE_PORT || process.env.PORT || 8080);
 ip = process.env.NODEJS_SERVICE_IP || "127.0.0.1" || "0.0.0.0";
 
-server.listen(port);
+server.listen(port, ip);
 
 function ping() {
   this.res.writeHead(200);
