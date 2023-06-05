cf2e73d0db191f84a4194d2c4cad98678e9f8336
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-04 21:27:36 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -43,7 +43,7 @@ function connect(callback){
 //var dbt = MongoClient.connect(uri, function(err, db) {;
 //var db = require('mongodb').Db;
 var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://bot:bot@ac-6ymsztq-shard-00-00.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-01.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-02.lmt2mtk.mongodb.net:27017/bothwellbot?ssl=true&replicaSet=atlas-9aqeym-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://bothwellbot:bothwellbot@ac-sid4gyy-shard-00-00.wlwecwj.mongodb.net:27017,ac-sid4gyy-shard-00-01.wlwecwj.mongodb.net:27017,ac-sid4gyy-shard-00-02.wlwecwj.mongodb.net:27017/?ssl=true&replicaSet=atlas-h447c6-shard-0&authSource=admin&retryWrites=true&w=majority";
 //MongoClient.connect(uri, function(err, client) {
 /*function connect(callback){
   mongoDB.connect("mongodb://bot:bot@ac-6ymsztq-shard-00-00.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-01.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-02.lmt2mtk.mongodb.net:27017/bothwellbot?ssl=true&replicaSet=atlas-9aqeym-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {  
