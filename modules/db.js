de9b7bb6fce070e42b02ac270978dd97c341281c
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-04 21:13:22 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -41,7 +41,7 @@ function connect(callback){
 }
 */
 //var dbt = MongoClient.connect(uri, function(err, db) {;
-var db = require('mongodb').Db;
+//var db = require('mongodb').Db;
 var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://bot:bot@ac-6ymsztq-shard-00-00.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-01.lmt2mtk.mongodb.net:27017,ac-6ymsztq-shard-00-02.lmt2mtk.mongodb.net:27017/bothwellbot?ssl=true&replicaSet=atlas-9aqeym-shard-0&authSource=admin&retryWrites=true&w=majority";
 //MongoClient.connect(uri, function(err, client) {
