19cb933bdce2ca5111806b9c7cd8ee17de8eaf34
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-04 22:49:38 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,5 +1,5 @@
 var mongoDB     = require('mongodb').MongoClient;
-//
+  var collection = client.db("test").collection("devices");
 var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 
@@ -33,7 +33,7 @@ exports.getAllDocuments = function(collection, callback) {
 
     if(err) throw err;
 
-    var allDocs = db.collection(collection).find().toArray(function(err, docs) {
+    var allDocs = collection.find().toArray(function(err, docs) {
 
       callback(docs);
 
@@ -49,7 +49,7 @@ exports.findDocs = function(collection, matchHash, callback) {
 
   connect(function(db){
 
-    var cursor = db.collection(collection).find(matchHash);
+    var cursor = collection.find(matchHash);
 
     var ret = [];
 
